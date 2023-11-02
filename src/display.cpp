e390268fc5e1fb441c8752731f2aec9dfd0aa69b
istif <istif@MSI>
2023-11-02 15:56:14 -0400

    cleaned up code a bit

--- src/display.cpp
+++ src/display.cpp
@@ -12,14 +12,25 @@ Display::Display(const char* title, int w, int h,SDL_Renderer* render,SDL_Window
     this->renderer = renderer;
     this->window = window;
 }
-Display::Display(const char* title, int w, int h,SDL_Renderer* render){
+Display::Display(const char* title, int w, int h){
     this->title = title;
     this->width = w;
     this->height = h;
-    this->renderer = renderer;
+    this->window = SDL_CreateWindow(this->title,50,50,w,h,SDL_WINDOW_RESIZABLE);
+    this->renderer = SDL_CreateRenderer(this->window,-1,0);
 }
-Display::cleanup(){
+void Display::update(){
+    SDL_SetRenderDrawColor(this->renderer, 3, 255, 255, 255);
+	SDL_RenderClear(this->renderer);
+	SDL_RenderPresent(this->renderer);
+}
+void Display::cleanup(){
 	SDL_DestroyWindow(this->window);
 	SDL_DestroyRenderer(this->renderer);
 	SDL_Quit();
+}
+
+void Display::windowResize(int w, int h){
+    this->width = w;
+    this->height = h;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,36 @@
 #include<SDL2/SDL.h>
-
 #include<iostream>
 #include<string>
 #include<game-engine/display.h>
 
 
-int WINDOW_WIDTH = 0;
-int WINDOW_HEIGHT = 0;
-int max_fps = 0;
-char* window_title = NULL;
+int WINDOW_WIDTH = 1280;
+int WINDOW_HEIGHT = 720;
+int max_fps = 120;
+const char* window_title = "Game-Engine Test";
 
-float delta_time;
 Display main_window;
-SDL_Renderer* renderer;
 
 void clean_up();
-
-<<<<<<< HEAD
 
 int main(int argc,char **argv){
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Event event;
 	SDL_PollEvent(&event);
 
-	// main_window = Display("Game-Engine Test", 1280, 720, );
-	// renderer = SDL_CreateRenderer(main_window, -1,0);
-	while (true)
-	{
-	SDL_Event event;
-	if (SDL_PollEvent(&event))
-	{
-		if (event.type == SDL_QUIT){break;}
-	}
-	// SDL_SetRenderDrawColor(renderer, 3, 255, 255, 255);
-	// SDL_RenderClear(renderer);
-	// SDL_RenderPresent(renderer);
+	main_window = Display(window_title,WINDOW_WIDTH,WINDOW_HEIGHT);
+
+	// GAME LOOP
+	while (true){
+		SDL_Event event;
+		if (SDL_PollEvent(&event)){
+			if (event.type == SDL_QUIT){break;}
+		}
+		main_window.update();
 	}
 	clean_up();
-=======
-int main(){
-	std::cout << "Fuck you Istifatty!" << std::endl;
->>>>>>> d219c172e78d0219166bd7df99dd91e8cfc7e740
 	return 0;		
 }
-
-void game_loop(){
-
-}
-void update(){}
-void render(){}
 void clean_up(){
 	main_window.cleanup();
 }
