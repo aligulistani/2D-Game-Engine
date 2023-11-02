e1bb0f35104c0aad1b1c75a4c8b508301af648a6
istif <istif@MSI>
2023-11-02 16:19:39 -0400

    Added main game engine class

---
+++ src/engineTester.cpp
@@ -0,0 +1,36 @@
+#include<SDL2/SDL.h>
+#include<iostream>
+#include<string>
+#include<game-engine/main.h>
+
+
+int WINDOW_WIDTH = 1280;
+int WINDOW_HEIGHT = 720;
+int max_fps = 120;
+const char* window_title = "Game-Engine Test";
+
+Display main_window;
+
+void clean_up();
+
+int main(int argc,char **argv){
+
+	GameEngine::initialize();
+
+
+	main_window = Display(window_title,WINDOW_WIDTH,WINDOW_HEIGHT);
+
+	// GAME LOOP
+	while (true){
+		SDL_Event event;
+		if (SDL_PollEvent(&event)){
+			if (event.type == SDL_QUIT){break;}
+		}
+		main_window.update();
+	}
+	clean_up();
+	return 0;		
+}
+void clean_up(){
+	main_window.cleanup();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,6 @@
+#include<game-engine/main.h>
 #include<SDL2/SDL.h>
-#include<iostream>
-#include<string>
-#include<game-engine/display.h>
-
-
-int WINDOW_WIDTH = 1280;
-int WINDOW_HEIGHT = 720;
-int max_fps = 120;
-const char* window_title = "Game-Engine Test";
-
-Display main_window;
-
-void clean_up();
-
-int main(int argc,char **argv){
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Event event;
-	SDL_PollEvent(&event);
-
-	main_window = Display(window_title,WINDOW_WIDTH,WINDOW_HEIGHT);
-
-	// GAME LOOP
-	while (true){
-		SDL_Event event;
-		if (SDL_PollEvent(&event)){
-			if (event.type == SDL_QUIT){break;}
-		}
-		main_window.update();
-	}
-	clean_up();
-	return 0;		
-}
-void clean_up(){
-	main_window.cleanup();
+GameEngine::GameEngine(){};
+void GameEngine::initialize(){
+    SDL_Init(SDL_INIT_EVERYTHING);
 }
