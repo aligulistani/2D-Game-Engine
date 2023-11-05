5944bb081ec0a023c7cc31416043ecff2e29afbb
istif <istif@MSI>
2023-11-04 22:36:46 -0400

    started working on gravity

--- src/engineTester.cpp
+++ src/engineTester.cpp
@@ -23,7 +23,7 @@ int main(int argc,char **argv){
 	GameEngine::initialize();
 
 	main_window = Display(window_title,WINDOW_WIDTH,WINDOW_HEIGHT);
-	rectangle = GameObject(0,0,25,25);
+	rectangle = GameObject(WINDOW_WIDTH/2,100,25,25);
 	scene = GameScene();
 
 	ninja = Texture("./res/ninja/Idle.png",main_window.renderer);
@@ -36,22 +36,24 @@ float velocity_x = 0.5f;
 float velocity_y = 0.5f;
 float speed = 1.0f;
 
-void calculate_angle(){
+float t_inital;
 
-}
-
+const float force_gravity = 4.0f;
 void game_loop(){
 	scene.addObjectToScene(&rectangle);
-	
-	float y = sin((SDL_GetTicks()));
+
+	// rectangle.setPos({SDL_GetTicks()/50.0f,200.0f*sin(rectangle.pos[0]/5.0f)+WINDOW_HEIGHT/2});
+	t_inital = SDL_GetTicks()/1000.0f;
+	float acceleration = force_gravity*t_inital;
+	rectangle.setVel(std::vector<float>{0.0f, acceleration});
 
 
-	rectangle.setPos({SDL_GetTicks()/50.0f,200.0f*sin(rectangle.pos[0]/5.0f)+WINDOW_HEIGHT/2});
+	std::cout << acceleration << std::endl;
 
-	// rectangle.setVel(std::vector<float>{0.05f,y});
-	std::cout << rectangle.pos[1] << std::endl;
 	if(rectangle.pos[1] >= WINDOW_HEIGHT ){// BOTTOM WALL
-		rectangle.pos[1] = WINDOW_HEIGHT-25;
+		rectangle.pos[1] = 0;
+		t_inital = 0;
+		acceleration = 0;
 	}else if(rectangle.pos[1] <= 0){ // TOP WALL
 		rectangle.pos[1] = 0;
 	}else if(rectangle.pos[0] >= WINDOW_WIDTH){// RIGHT WALL
